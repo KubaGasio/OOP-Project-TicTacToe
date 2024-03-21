d95af0614af209154e863d8dfc6d0808e7572154
KubaGasio <163890021+KubaGasio@users.noreply.github.com>
2024-03-20 21:35:52 -0400

    Update Game1.cpp

--- Game1.cpp
+++ Game1.cpp
@@ -1,70 +1,35 @@
 #include "Game.h" 
 
 Game::Game() {
-
-
-
     WCounter = 0;
-
-
-
 }
-
-
-
 bool Game::CheckWin() {
-
     if (GameBoard[0][0] == GameBoard[0][1] && GameBoard[0][1] == GameBoard[0][2]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[1][0] == GameBoard[1][1] && GameBoard[1][1] == GameBoard[1][2]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[2][0] == GameBoard[2][1] && GameBoard[2][1] == GameBoard[2][2]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[0][0] == GameBoard[1][1] && GameBoard[1][1] == GameBoard[2][2]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[0][1] == GameBoard[1][1] && GameBoard[1][1] == GameBoard[2][1]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[0][2] == GameBoard[1][2] && GameBoard[1][2] == GameBoard[2][2]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[0][0] == GameBoard[1][0] && GameBoard[1][0] == GameBoard[2][0]) {
-
         Win = true;
-
     }
-
     else if (GameBoard[0][2] == GameBoard[1][1] && GameBoard[1][1] == GameBoard[2][0]) {
-
         Win = true;
-
     }
     else {
-
         Win = false;
-
     }
 }
 void Game::DrawBoard() {
